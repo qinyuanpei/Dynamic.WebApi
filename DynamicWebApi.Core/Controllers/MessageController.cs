e8491049693abd90e8aa96b60ebeaf2bf258ed3b
payneqin <payneqin@qt-asia.com>
2019-10-16 10:01:54 +0800

    :fire: 基于MsgPack的Rpc

--- DynamicWebApi.Core/Controllers/MessageController.cs
+++ DynamicWebApi.Core/Controllers/MessageController.cs
@@ -1,5 +1,6 @@
 ﻿using Grpc.Core;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,15 @@ namespace DynamicWebApi.Core.Controllers
 {
     public class MessageController
     {
+        private readonly ILogger<MessageController> _logger;
+        public MessageController(ILogger<MessageController> logger)
+        {
+            _logger = logger;
+        }
+
         public string Echo(string receiver)
         {
+            _logger.LogInformation($"Invoke Echo with: {receiver}");
             return $"Hello, {receiver}";
         }
     }
